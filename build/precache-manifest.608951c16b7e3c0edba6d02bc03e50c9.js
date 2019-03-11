6a867697e1f042c23c0f48694783ecff0d836803
yys-0505 <yinyishuai@126.com>
2019-03-11 15:28:15 +0800

    first commit

---
+++ build/precache-manifest.608951c16b7e3c0edba6d02bc03e50c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4243916ee02d1f4174e114e79214057",
+    "url": "./static/media/detail.e4243916.jpg"
+  },
+  {
+    "revision": "9efd8fecee0af0e45a96df47bddb1203",
+    "url": "./static/media/banner.9efd8fec.jpeg"
+  },
+  {
+    "revision": "09011a07d15b007b6844",
+    "url": "./static/js/runtime~main.09011a07.js"
+  },
+  {
+    "revision": "1745db601f81af6ef5a1",
+    "url": "./static/js/main.1745db60.chunk.js"
+  },
+  {
+    "revision": "1f4584e8278993eeeb63",
+    "url": "./static/js/3.1f4584e8.chunk.js"
+  },
+  {
+    "revision": "2bf116efb42f5b94c908",
+    "url": "./static/js/2.2bf116ef.chunk.js"
+  },
+  {
+    "revision": "c8a82834c1026a6020cb9234c08e94f5",
+    "url": "./index.html"
+  }
+];
